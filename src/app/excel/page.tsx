4435bf324f27c954fca4ca2924f35557c88abcfd
Hongbin Wang <wanghongbin99@gmail.com>
2025-05-31 14:12:10 +0800

    about

---
+++ src/app/excel/page.tsx
@@ -0,0 +1,10 @@
+function about() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold mb-4">Excel Page</h1>
+      <p className="text-lg">This is the Excel page.</p>
+    </div>
+  );
+}   
+
+export default about;
